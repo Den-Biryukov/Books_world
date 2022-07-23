aac4e64ea15096abab2eb197ee49e53cdb99608f
denis <den-biryukov-2016@mail.ru>
2022-07-23 18:32:25 +0300

    added new model UserBookRelation

--- read_book/models.py
+++ read_book/models.py
@@ -11,3 +11,19 @@ class Book(models.Model):
 
     def __str__(self):
         return f'id - {self.id}: name - {self.name}'
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'Very bad'),
+        (2, 'Bad'),
+        (3, 'OK'),
+        (4, 'COOL'),
+        (5, 'AMAZING')
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    is_favorites = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
