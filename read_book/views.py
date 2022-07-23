af133d3e4afe6d4eeab2b1cbf7433947b5826b12
denis <den-biryukov-2016@mail.ru>
2022-07-23 15:21:24 +0300

    modified tests and permissions

--- read_book/views.py
+++ read_book/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from read_book.models import Book
-from read_book.permissions import IsOwnerOrReadOnly
+from read_book.permissions import IsOwnerOrStaffOrReadOnly
 from read_book.serializers import BooksSerializer
 
 
@@ -13,7 +12,7 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     filter_fields = ['name', 'price', 'author_name'] # по каким полям хотим фильтровать
     search_fields = ['name', 'author_name'] # по каким полям хотим искать
     ordering_fields = ['name', 'price', 'author_name'] # сортировка
