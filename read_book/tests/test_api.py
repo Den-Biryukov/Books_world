565bf82f47a7ada61bdb2642579add8d99c061a1
denis <den-biryukov-2016@mail.ru>
2022-07-23 15:12:27 +0300

    added custom permission and modified tests

--- read_book/tests/test_api.py
+++ read_book/tests/test_api.py
@@ -13,7 +13,8 @@ class BooksAPITestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create(username='test_username', )
         self.book_1 = Book.objects.create(name='Test book 1', price=25,
-                                          author_name='Author 1')
+                                          author_name='Author 1',
+                                          owner=self.user)
         self.book_2 = Book.objects.create(name='Test book 2', price=50,
                                           author_name='Author 2')
         self.book_3 = Book.objects.create(name='Test book Author 1', price=8,
@@ -53,6 +54,7 @@ class BooksAPITestCase(APITestCase):
 
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(4, Book.objects.all().count())
+        self.assertEqual(self.user, Book.objects.last().owner)
 
     def test_update(self):
         url = reverse('book-detail', args=(self.book_1.id,))
@@ -69,3 +71,21 @@ class BooksAPITestCase(APITestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.book_1.refresh_from_db()
         self.assertEqual(57, self.book_1.price)
+
+    def test_update_not_owner(self):
+        self.user2 = User.objects.create(username='test_username2', )
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 57,
+            "author_name": self.book_1.author_name
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user2)
+        response = self.client.put(url, data=json_data,
+                                   content_type='application/json')
+
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+        self.book_1.refresh_from_db()
+        self.assertEqual(25, self.book_1.price)
+
--- read_book/views.py
+++ read_book/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 from rest_framework.viewsets import ModelViewSet
 
 from read_book.models import Book
+from read_book.permissions import IsOwnerOrReadOnly
 from read_book.serializers import BooksSerializer
 
 
@@ -12,10 +13,15 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
     filter_fields = ['name', 'price', 'author_name'] # по каким полям хотим фильтровать
     search_fields = ['name', 'author_name'] # по каким полям хотим искать
     ordering_fields = ['name', 'price', 'author_name'] # сортировка
+
+    def perform_create(self, serializer):
+        serializer.validated_data['owner'] = self.request.user
+        serializer.save()
+
 
 
 def auth(request):
