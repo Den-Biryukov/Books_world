f8293e688409355233ddf6c47b00e403ee0239ea
denis <den-biryukov-2016@mail.ru>
2022-07-25 15:18:47 +0300

    test is_favorites_count

--- read_book/serializers.py
+++ read_book/serializers.py
@@ -4,17 +4,24 @@ from read_book.models import Book, UserBookRelation
 
 
 class BooksSerializer(serializers.ModelSerializer):
+
     likes_count = serializers.SerializerMethodField()
+    is_favorites_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
         # fields = '__all__' # обработка всех позиций
         # fields = ['price'] # обработка только позиции price
-        fields = ('id', 'name', 'price', 'author_name', 'likes_count')
+        fields = ('id', 'name', 'price', 'author_name', 'likes_count',
+                  'is_favorites_count')
 
     def get_likes_count(self, instance):
         return UserBookRelation.objects.filter(book=instance,
                                                like=True).count()
+
+    def get_is_favorites_count(self, instance):
+        return UserBookRelation.objects.filter(book=instance,
+                                               is_favorites=True).count()
 
 
 class UserBookRelationSerializer(serializers.ModelSerializer):
--- read_book/tests/test_serializers.py
+++ read_book/tests/test_serializers.py
@@ -15,6 +15,7 @@ class BookSerializerTestCase(TestCase):
         book_2 = Book.objects.create(name='Test book 2', price=50,
                                      author_name='Author 2')
 
+        # Test likes_count for book_1 and book_2
         UserBookRelation.objects.create(user=user1, book=book_1, like=True)
         UserBookRelation.objects.create(user=user2, book=book_1, like=True)
         UserBookRelation.objects.create(user=user3, book=book_1, like=True)
@@ -23,6 +24,15 @@ class BookSerializerTestCase(TestCase):
         UserBookRelation.objects.create(user=user2, book=book_2, like=True)
         UserBookRelation.objects.create(user=user3, book=book_2, like=False)
 
+        # Test is_favorites_count for book_1 and book_2
+        UserBookRelation.objects.create(user=user1, book=book_1, is_favorites=True)
+        UserBookRelation.objects.create(user=user2, book=book_1, is_favorites=True)
+        UserBookRelation.objects.create(user=user3, book=book_1, is_favorites=True)
+
+        UserBookRelation.objects.create(user=user1, book=book_2, is_favorites=True)
+        UserBookRelation.objects.create(user=user2, book=book_2, is_favorites=True)
+        UserBookRelation.objects.create(user=user3, book=book_2, is_favorites=False)
+
         data = BooksSerializer([book_1, book_2], many=True).data
         expected_data = [
             {
@@ -30,14 +40,16 @@ class BookSerializerTestCase(TestCase):
                 'name': 'Test book 1',
                 'price': '25.00',
                 'author_name': 'Author 1',
-                'likes_count': 3
+                'likes_count': 3,
+                'is_favorites_count': 3
             },
             {
                 'id': book_2.id,
                 'name': 'Test book 2',
                 'price': '50.00',
                 'author_name': 'Author 2',
-                'likes_count': 1
+                'likes_count': 1,
+                'is_favorites_count': 2
             },
         ]
         self.assertEqual(expected_data, data)
